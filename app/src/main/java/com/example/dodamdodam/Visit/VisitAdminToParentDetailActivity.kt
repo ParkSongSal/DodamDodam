3b7368ee3ba549b12933549afb2020cc78881a5f
SangMook Park <tkdanr0311@gmail.com>
2022-05-01 21:31:31 +0900

    # 관리자 - 각 보호자별 게시물 목록 추가

---
+++ app/src/main/java/com/example/dodamdodam/Visit/VisitAdminToParentDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dodamdodam.Visit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dodamdodam.R
+
+class VisitAdminToParentDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visit_admin_to_parent_detail)
+    }
+}
